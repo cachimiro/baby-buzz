0975bf3ff026f885ab5d1c009122ef7e565378ee
cachimiro <johannaguirre55@gmail.com>
2020-09-04 10:49:48 +0000

    fixing bug on secret key

--- baby_buzz/settings.py
+++ baby_buzz/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY','')
+SECRET_KEY = os.environ.get('SECRET_KEY','') 
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['johann-baby-buzz.herokuapp.com', 'localhost']
 
