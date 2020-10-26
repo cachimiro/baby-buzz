f57940e526d9966dbbe635d2ea2fb87b33b2921b
cachimiro <johannaguirre55@gmail.com>
2020-10-26 09:45:46 +0000

    fixing heroku bugs

--- cart/templatetags/bag_tools.py
+++ cart/templatetags/bag_tools.py
@@ -2,7 +2,7 @@ from django import template
 
 
 register = template.Library()
-
+#code to calculate the subtotal
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
     return price * quantity
